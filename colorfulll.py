1c349b768ca88a12a1e1e60e2c0a97e8ff3a8fa3
shwetabhumannavar04-coder <shwetabhumannavar04@gmail.com>
2026-08-02 23:28:00 +0530

    Add files via upload

---
+++ colorfulll.py
@@ -0,0 +1,29 @@
+import turtle
+import math
+import random
+screen = turtle.Screen()
+screen.bgcolor("sky blue")
+screen.tracer(2)
+t = turtle.Turtle()
+t.speed()
+t.hideturtle()
+t.pensize(1)
+colors = ["white","pink"]
+for i in range(120):
+    t.penup()
+    t.goto(0, 40)
+    angle = i * (math.pi * 2) / 120
+    x = 16 * (math.sin(angle) ** 3)* 15
+    y = (13 * math.cos(angle) - 5 *
+math.cos(2 * angle) - 2 * math.cos(3 *
+angle) - math.cos(4 * angle)) * 15
+    c= random.choice(colors)
+    t.color(c)
+    t.pendown()
+    t.goto(x, y)
+    for _  in range(8):
+        t.forward(6)
+        t.backward(6)
+        t.right(45)
+screen.update()
+turtle.done()
